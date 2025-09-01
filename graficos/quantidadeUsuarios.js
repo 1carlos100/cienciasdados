3c36b3f361fe1fbd817a677f073e5054ab6d18a7
1carlos100 <carlos.emanuel.silva@escola.pr.gov.br>
2025-09-01 11:23:00 -0300

    Update quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,26 +1,24 @@
-const url="https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json"
-
+const url = "https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json"
 
 async function vizualizarInformacoesGlobais() {
+    try {
+        const resposta = await fetch(url)
+        const dados = await resposta.json()
 
-    const resposta = await fetch(url)
+        const pessoasMundo = (dados.total_pessoas_mundo / 1e9)
+        const trabalhadoresMundo = (dados.total_trabalhadores_mundo / 1e9)
+        const tempoDesTrabalho = parseInt(dados.tempo_medio_deslocamento_para_trabalho)
+        const minutos = Math.round((dados.tempo_medio_deslocamento_para_trabalho - tempoDesTrabalho) * 60)
 
-    const dados = await resposta.json()
+        const paragrafo = document.createElement('p')
+        paragrafo.classList.add('graficos-container_texto')
 
-    const pessoasMundo = (dados.total_pessoas_mundo/1e9)
-    const trabalhadoresMundo = (dados.total_trabalhadores_mundo/1e9)
-    const tempoDesTrabalho = parseInt (dados.tempo_medio_deslocamento_para_trabalho)
-    const minutos = Math.round((dados.tempo_medio_deslocamento_para_trabalho - tempoDesTrabalho) * 60)
+        paragrafo.innerHTML = `O mundo tem <span>${pessoasMundo.toFixed(2)}</span> bilhões de pessoas, dessas pessoas, aproximadamente <span>${trabalhadoresMundo.toFixed(2)}</span> bilhões estão empregadas e passam em média <span>${tempoDesTrabalho} horas e ${minutos} minutos</span> se deslocando para o trabalho.`
 
-
-
-const paragrafo= document.createElement('p')
-
-paragrafo.classilist.add('graficos-container_texto')
-
-paragrafo.innerHTML = `O mundo tem <span>${pessoasMundo}</span>  bilhões de pessoas, dessas pessoas, aproximadamente <span>${trabalhadoresMundo}</span>   bilhões estão empregadas e passam em média  <span>${tempoDesTrabalho}horas</span> e <span>${minutos} minutos</span> por dia no caminho para o trabalho. Temos, portanto, mais da metade da população mundial que não exerce trabalhos com vínculos empregatícios legais. O que pode significar que há longevidade e desse modo, muitas pessoas aposentadas. Porém, também pode significar muitos postos de trabalho chamados informais.`
-
-const container=document.getElementById('graficos-container')
-container.appendChild(paragrafo);
-
+        const container = document.getElementById('graficos-container')
+        container.appendChild(paragrafo)
+    } catch (e) {
+        console.error("Erro ao buscar ou processar dados globais:", e)
+    }
 }
+
